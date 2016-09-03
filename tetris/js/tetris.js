78ca1dfce8af164f4ac5118a8ac065b9fd7653e3
Igor Degtyarev <zanooda@ya.ru>
2016-09-04 01:20:33 +0300

    add swipe

--- tetris/js/tetris.js
+++ tetris/js/tetris.js
@@ -165,6 +165,48 @@ var point = null;
                 break;
         }
     };
+
+    // swipe
+    document.addEventListener('touchstart', handleTouchStart, false);
+    document.addEventListener('touchmove', handleTouchMove, false);
+
+    var xDown = null;
+    var yDown = null;
+
+    function handleTouchStart(evt) {
+        xDown = evt.touches[0].clientX;
+        yDown = evt.touches[0].clientY;
+    };
+
+    function handleTouchMove(evt) {
+        if (!xDown || !yDown) {
+            return;
+        }
+
+        var xUp = evt.touches[0].clientX;
+        var yUp = evt.touches[0].clientY;
+
+        var xDiff = xDown - xUp;
+        var yDiff = yDown - yUp;
+
+        if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
+            if (xDiff > 0) {
+                left();
+            } else {
+                right();
+            }
+        } else {
+            if (yDiff > 0) {
+                rotate();
+            } else {
+                fall();
+            }
+        }
+        /* reset values */
+        xDown = null;
+        yDown = null;
+    };
+    //
 })();
 
 function redrawState() {
